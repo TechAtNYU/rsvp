725ae5775dc9c5c40935b924d5a9a687faabe7c5
Julie Pan <ycp217@nyu.edu>
2015-09-17 18:05:09 -0400

    set up ajax call to tnyu api

--- src/server/server.js
+++ src/server/server.js
@@ -17,7 +17,7 @@ app.get('/*', function (req, res) {
   });
 });
 
-var server = app.listen(5000, function () {  
+var server = app.listen(3000, function () {  
   var host = server.address().address;
   var port = server.address().port;
 
--- src/shared/components/AppHandler.js
+++ src/shared/components/AppHandler.js
@@ -1,7 +1,60 @@
 import React from "react";
 
-export default class AppHandler extends React.Component {  
+class DropDownMenu extends React.Component {
+  constructor() {
+    super();
+    this.state = { checked: false };
+  }
+
+  _toggleCheckbox() {
+    console.log('wut');
+  }
+
   render() {
-    return <div>Hello App Handler</div>;
+    return(
+      <div>
+      <div className="col-md-6">
+      <h3 className="text-center">Basic Example</h3>
+      <div className="well">
+      <div className="list-group">
+      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked={this.state.checked} type="checkbox"/> Hi</div>
+      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
+      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
+      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
+      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
+      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
+      </div>
+      </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+
+export default class AppHandler extends React.Component {
+  constructor() {
+    super();
+
+  }
+  
+  _jsonpCalback(result) {
+    console.log(result);
+  }
+
+  componentDidMount() {
+    $.getJSON("https://api.tnyu.org/v2/people/me")
+      .done(function(user) {
+        console.log(user);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+      <DropDownMenu />
+      </div>
+
+    );
   }
 }
