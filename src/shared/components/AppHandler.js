4cad06099c06982a1f77e4badb4bca9b2ecec69a
Julie Pan <ycp217@nyu.edu>
2015-09-18 17:08:02 -0400

    load events and generate menu items

--- src/shared/components/AppHandler.js
+++ src/shared/components/AppHandler.js
@@ -1,31 +1,48 @@
 import React from "react";
 
+class MenuItem extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        checked: false
+      };
+    }
+
+    _toggleCheckbox() {
+      let ret = (this.state.checked) ? false: true;
+      this.setState({
+        checked: ret
+      });
+    }
+
+    render() {
+      return(
+        <div className="list-group-item event-list-item" onClick={this._toggleCheckbox.bind(this)} ><input type="checkbox" checked={ this.state.checked }/> {this.props.title }</div>
+      );
+    }
+}
+
 class DropDownMenu extends React.Component {
   constructor() {
     super();
-    this.state = { checked: false };
   }
 
-  _toggleCheckbox() {
-    console.log('wut');
-  }
 
   render() {
+    let itemNodes = this.props.events.map( (event) => {
+      return (
+        <MenuItem title={event.attributes.title} />
+      );
+    });
+
     return(
-      <div>
       <div className="col-md-6">
       <h3 className="text-center">Basic Example</h3>
-      <div className="well">
-      <div className="list-group">
-      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked={this.state.checked} type="checkbox"/> Hi</div>
-      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
-      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
-      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
-      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
-      <div className="list-group-item" onClick={ this._toggleCheckbox }><input checked="true" type="checkbox"/> Hi</div>
-      </div>
-      </div>
-      </div>
+        <div className="well">
+          <div className="list-group">
+            {itemNodes}
+          </div>
+        </div>
       </div>
     )
   }
@@ -35,26 +52,31 @@ class DropDownMenu extends React.Component {
 export default class AppHandler extends React.Component {
   constructor() {
     super();
-
+    this.state = {
+      events: [],
+      userId: ''
+    };
   }
   
-  _jsonpCalback(result) {
-    console.log(result);
-  }
-
   componentDidMount() {
-    $.getJSON("https://api.tnyu.org/v2/people/me")
-      .done(function(user) {
-        console.log(user);
+    // $.getJSON('https://api.tnyu.org/v2/people/me')
+    //   .done( function(user) {
+    //     console.log(user.data);
+    //   });
+
+    $.getJSON('https://api.tnyu.org/v2/events/next-10-publicly')
+      .done( (json) => {
+        this.setState({
+          events: json.data
+        });
       });
   }
 
   render() {
     return (
       <div>
-      <DropDownMenu />
+      <DropDownMenu events={this.state.events}/>
       </div>
-
     );
   }
 }
