edadf6b79be7df23ab41dd23d7947e5b524579cc
Julie Pan <ycp217@nyu.edu>
2015-09-24 19:45:01 -0400

    add eventId selection

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ RSVP to one of our events
 
 To start (cross-domain needs to run on localhost:3000):
 python server.py
+
+- add N# and preferred email if can't get from /people/me
+
+
--- main.js
+++ main.js
@@ -1,75 +1,89 @@
-  var MenuItem = React.createClass({
-    getInitialState: function() {
-      return { checked: false };
-    },
+var DropDownMenu = React.createClass({
+  getInitialState: function() {
+    return {
+      selectedEvents: {}
+    };
+  },
 
-    _toggleCheckbox: function() {
-      var ret = (this.state.checked) ? false: true;
-      this.setState({
-        checked: ret
-      });
-    },
+  _getRSVP: function(event) {
+    event.preventDefault();
+  },
 
-    render: function() {
-      return(
-        <div className="list-group-item event-list-item" onClick={this._toggleCheckbox} >
-        <input type="checkbox" checked={ this.state.checked } />
-        {this.props.title }</div>
-      );
+  _handleChange: function(i) {
+    var id = this.props.eventIds[i];
+    var selected = this.state.selectedEvents;
+
+    if (id in selected) {
+      selected[id] = (selected[id]) ? false: true;
+    } else {
+      selected[id] = true;
     }
-  });
 
-  var DropDownMenu = React.createClass({
-    render: function() {
-      var itemNodes = this.props.events.map( (event) => {
-        return (
-          <MenuItem title={event.attributes.title} />
-        );
-      });
-
-      return(
-        <div className="col-md-6">
-        <h3 className="text-center">Basic Example</h3>
-        <div className="well">
-        <div className="list-group">
-        {itemNodes}
-        </div>
-        </div>
-        </div>
-      )
-    }
-  });
-
-
-  var AppHandler = React.createClass({
-    getInitialState: function() {
-      return {
-        events: [],
-        userId: ''
-      };
-    },
-
-    componentDidMount: function() {
-      // $.getJSON('https://api.tnyu.org/v2/people/me')
-      //   .done( function(user) {
-      //     console.log(user.data);
-      //   });
-
-      $.getJSON('https://api.tnyu.org/v2/events/next-10-publicly')
-      .done( (json) => {
-        this.setState({
-          events: json.data
-        });
-      });
-    },
-
-    render: function() {
+    this.setState({
+      selectedEvents: selected
+    });
+  },
+  render: function() {
+    var itemNodes = this.props.eventTitles.map( (title, i) => {
       return (
-        <div>
-        <DropDownMenu events={this.state.events}/>
+        <div key={i} className="list-group-item event-list-item">
+        <input type="checkbox" key={i} onChange={this._handleChange.bind(this, i)} />
+        <span> { title }</span>
         </div>
       );
-    }
-  });
+    });
 
-  React.render(<AppHandler />, document.getElementById('app'));
+    return(
+      <div className="col-md-6">
+        <h3 className="text-center">Basic Example</h3>
+          <div className="well">
+            <div className="list-group">
+              {itemNodes}
+            </div>
+          </div>
+
+      <button onClick={this._getRSVP}>RSVP</button>
+      </div>
+    )
+  }
+});
+
+
+var AppHandler = React.createClass({
+  getInitialState: function() {
+    return {
+      userId: '',
+      eventTitles: [],
+      eventIds: [],
+      rawJson: []
+    };
+  },
+
+  componentWillMount: function() {
+    // $.getJSON('https://api.tnyu.org/v2/people/me')
+    //   .done( function(user) {
+    //     console.log(user.data);
+    //   });
+
+    $.getJSON('https://api.tnyu.org/v2/events/next-10-publicly')
+    .done( (json) => {
+      var eventIds = json.data.map(function(event) { return event.id; });
+      var eventTitles = json.data.map(function(event) { return event.attributes.title; });
+      this.setState({
+        eventTitles: eventTitles,
+        eventIds: eventIds,
+        rawJson: json.data
+      });
+    });
+  },
+
+  render: function() {
+    return (
+      <div>
+      <DropDownMenu eventTitles={this.state.eventTitles} eventIds={this.state.eventIds} rawJson={this.state.rawJson}/>
+      </div>
+    );
+  }
+});
+
+React.render(<AppHandler />, document.getElementById('app'));
