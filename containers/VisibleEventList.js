be8944b5d850eb44c7e44273a01a9480f23ef876
Julie Pan <ycp217@nyu.edu>
2016-03-20 12:40:07 -0400

    save prog

--- containers/VisibleEventList.js
+++ containers/VisibleEventList.js
@@ -5,15 +5,15 @@ import EventList from '../components/EventList';
 const mapStateToProps = (state) => {
     return {
         events: state.eventActions.events
-    };
-};
+    }
+}
 
 const mapDispatchToProps = (dispatch) => {
     return {
         onEventClick: (id) => dispatch(toggleEvent(id)),
         onRsvpClick: () => dispatch(rsvpToEvents())
-    };
-};
+    }
+}
 
 const VisibleEventList = connect(
     mapStateToProps,
